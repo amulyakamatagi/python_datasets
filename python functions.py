6b6b5f2913a2fd4df72f79e5fc4418119fa74721
amulyak <amulyak697@gmail.com>
2023-08-03 18:53:08 +0530

    commiting the python functions file v1

---
+++ python functions.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 15:05:32 2023
+
+@author: amuly
+"""
+
+
+#FUNCTIONS
+#A MECHANISM WHICH KEEPS EXECUTING 
+
+def f1(x):
+    y=x**2
+    return y
+f1(2)
+f1(3)
+
+def f2(x):
+    y=x**3
+    return y
+
+def f3(z):
+    y=z*5
+    return y
+
+f3(7)
+f3(10)  
+
+def f9(x):
+    y=x/2
+    return y
+f9(4)
+round(f9(4))
+
+sum(f9(4))
+
+def f2(x):
+    y=x+2
+    return y
+f2(2)
+f2(-3)
+f2(90)
+
+
+def f3(x):
+    y=2*x+1
+    return y
+
+f3(3)
+f3(0)
+f3(90)
+
+def f5(y,z):
+    x=y+z/4
+    return y
+
+f5(5,6)
+f5(2,3)
+
+x='salary'
+x=[1000,2000,3000,4000,5000]
+def x1(y):
+    z=y+3000
+    return z
+x1(1000)
+x1(2000)
+x1(3000)
+x1(4000)
+x1(5000)
+
+
+
+    
+
